12d46adc0c4e0e36f648b02cccc058a47fa4761c
Themer97 <themersaanouni@gmail.com>
2022-10-31 15:16:36 +0100

    first commit

---
+++ props/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './Components/Card'
+import './Components/Card.css'
+
+function App() {
+  const name="Themer Saanouni";
+  const job="Front-end Developer";
+  const about="As a React developer, i'll help to develop components of the user interface and implement them through React.js workflows like Redux or Flux. Additionally, i'll make sure that those user interface components are easy to maintain and robust. ";
+
+  return (
+    <div className="App">
+      <Card name={name} job={job} about={about} />
+    </div>
+  );
+}
+
+export default App;
